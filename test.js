ccd14df6fc6aa4ebb09809edeee212158a7a5c3e
asherpatience1 <asherpatience1@gmail.com>
2019-08-31 17:27:23 +0700

    update

--- test.js
+++ test.js
@@ -30,6 +30,40 @@ var light = new THREE.DirectionalLight(0xffeedd);
 light.position = new THREE.Vector3(0,1000,0);   
 scene.add(light); 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var loader = new THREE.ObjectLoader();
+
+loader.load(
+	// resource URL
+	"bunny.obj",
+
+	// onLoad callback
+	// Here the loaded data is assumed to be an object
+	function ( obj ) {
+		// Add the loaded object to the scene
+		scene.add( obj );
+	},
+
+	// onProgress callback
+	function ( xhr ) {
+		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+	},
+
+	// onError callback
+	function ( err ) {
+		console.error( 'An error happened' );
+	}
+);
+
+
+// Alternatively, to parse a previously loaded JSON structure
+var object = loader.parse( a_json_object );
+
+scene.add( object );
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 var animate = function () {
 
     cube.rotation.x = 45;
