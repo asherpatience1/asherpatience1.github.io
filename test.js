0d19ed4d437f3d8daab655be0f839a33ec2f3eb4
asherpatience1 <asherpatience1@gmail.com>
2019-08-31 17:30:33 +0700

    Update test.js

--- test.js
+++ test.js
@@ -32,35 +32,35 @@ scene.add(light);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var loader = new THREE.ObjectLoader();
+// var loader = new THREE.ObjectLoader();
 
-loader.load(
-	// resource URL
-	"bunny.obj",
+// loader.load(
+// 	// resource URL
+// 	"bunny.obj",
 
-	// onLoad callback
-	// Here the loaded data is assumed to be an object
-	function ( obj ) {
-		// Add the loaded object to the scene
-		scene.add( obj );
-	},
+// 	// onLoad callback
+// 	// Here the loaded data is assumed to be an object
+// 	function ( obj ) {
+// 		// Add the loaded object to the scene
+// 		scene.add( obj );
+// 	},
 
-	// onProgress callback
-	function ( xhr ) {
-		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-	},
+// 	// onProgress callback
+// 	function ( xhr ) {
+// 		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+// 	},
 
-	// onError callback
-	function ( err ) {
-		console.error( 'An error happened' );
-	}
-);
+// 	// onError callback
+// 	function ( err ) {
+// 		console.error( 'An error happened' );
+// 	}
+// );
 
 
-// Alternatively, to parse a previously loaded JSON structure
-var bunny = loader.parse( a_json_object );
+// // Alternatively, to parse a previously loaded JSON structure
+// var bunny = loader.parse( a_json_object );
 
-scene.add( bunny );
+// scene.add( bunny );
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
