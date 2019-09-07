2aaef159b5cc1784299d75c474cd61e4273f3508
asherpatience1 <asherpatience1@gmail.com>
2019-09-07 15:34:28 +0700

    Update theSite.js

--- theSite.js
+++ theSite.js
@@ -1,5 +1,5 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(72, window.innerWidth/window.innerHeight, 0.1, 10000);
+var camera = new THREE.PerspectiveCamera(35, window.innerWidth/window.innerHeight, 0.1, 10000);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -28,15 +28,16 @@ function onMouseMove( event ) {
 	// (-1 to +1) for both components
 
 	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-camera.position.z = -10;
-camera.position.y = 3;
-camera.lookAt(new THREE.Vector3(0,-5,0));
+camera.position.z = 36;
+camera.position.y = 6;
+camera.position.x = 10;
+camera.lookAt(new THREE.Vector3(10,-12,0));
 
 var ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5);
 scene.add(ambientLight);
@@ -68,35 +69,36 @@ var mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 var tick = 0;
-var xSize = 20;
-var ySize = 20;
+var xSize = 40;
+var ySize = 70;
 var startPos = [];
 var targetPos = [];
 var smoothRate = 0.05;
+var scaler = 0.5;
 for (var x = 0;x < xSize;x++){
 
     for (var y = 0;y < ySize;y++){
 
-        geometry.vertices.push(new THREE.Vector3((x)-(xSize/2),0,(y)-(ySize/2)));
-        startPos.push(new THREE.Vector3((x)-(xSize/2),0,(y)-(ySize/2)));
-        targetPos.push(new THREE.Vector3((x)-(xSize/2),0,(y)-(ySize/2)));
-        geometry.vertices.push(new THREE.Vector3((x)-(xSize/2),0,(y)-(ySize/2)));
-        startPos.push(new THREE.Vector3((x)-(xSize/2),0,(y+1)-(ySize/2)));
-        targetPos.push(new THREE.Vector3((x)-(xSize/2),0,(y+1)-(ySize/2)));
-        geometry.vertices.push(new THREE.Vector3((x)-(xSize/2),0,(y)-(ySize/2)));
-        startPos.push(new THREE.Vector3((x+1)-(xSize/2),0,(y+1)-(ySize/2)));
-        targetPos.push(new THREE.Vector3((x+1)-(xSize/2),0,(y+1)-(ySize/2)));
+        geometry.vertices.push(new THREE.Vector3((x*scaler),0,(y*scaler)));
+        startPos.push(new THREE.Vector3((x*scaler),0,(y*scaler)));
+        targetPos.push(new THREE.Vector3((x*scaler),0,(y*scaler)));
+        geometry.vertices.push(new THREE.Vector3((x*scaler),0,(y*scaler)));
+        startPos.push(new THREE.Vector3((x*scaler),0,(y*scaler+1)));
+        targetPos.push(new THREE.Vector3((x*scaler),0,(y*scaler+1)));
+        geometry.vertices.push(new THREE.Vector3((x*scaler),0,(y*scaler)));
+        startPos.push(new THREE.Vector3((x*scaler+1),0,(y*scaler+1)));
+        targetPos.push(new THREE.Vector3((x*scaler+1),0,(y*scaler+1)));
         geometry.faces.push(new THREE.Face3(tick, tick+1, tick+2));
 
-        geometry.vertices.push(new THREE.Vector3((x)-(xSize/2),0,(y)-(ySize/2)));
-        startPos.push(new THREE.Vector3((x)-(xSize/2),0,(y)-(ySize/2)));
-        targetPos.push(new THREE.Vector3((x)-(xSize/2),0,(y)-(ySize/2)));
-        geometry.vertices.push(new THREE.Vector3((x)-(xSize/2),0,(y)-(ySize/2)));
-        startPos.push(new THREE.Vector3((x+1)-(xSize/2),0,(y+1)-(ySize/2)));
-        targetPos.push(new THREE.Vector3((x+1)-(xSize/2),0,(y+1)-(ySize/2)));
-        geometry.vertices.push(new THREE.Vector3((x)-(xSize/2),0,(y)-(ySize/2)));
-        startPos.push(new THREE.Vector3((x+1)-(xSize/2),0,(y)-(ySize/2)));
-        targetPos.push(new THREE.Vector3((x+1)-(xSize/2),0,(y)-(ySize/2)));
+        geometry.vertices.push(new THREE.Vector3((x*scaler),0,(y*scaler)));
+        startPos.push(new THREE.Vector3((x*scaler),0,(y*scaler)));
+        targetPos.push(new THREE.Vector3((x*scaler),0,(y*scaler)));
+        geometry.vertices.push(new THREE.Vector3((x*scaler),0,(y*scaler)));
+        startPos.push(new THREE.Vector3((x*scaler+1),0,(y*scaler+1)));
+        targetPos.push(new THREE.Vector3((x*scaler+1),0,(y*scaler+1)));
+        geometry.vertices.push(new THREE.Vector3((x*scaler),0,(y*scaler)));
+        startPos.push(new THREE.Vector3((x*scaler+1),0,(y*scaler)));
+        targetPos.push(new THREE.Vector3((x*scaler+1),0,(y*scaler)));
         geometry.faces.push(new THREE.Face3(tick+3, tick+4, tick+5));
 
         tick += 6;
@@ -105,26 +107,54 @@ for (var x = 0;x < xSize;x++){
 
 }
 
+var raycaster = new THREE.Raycaster();
+var rayPos = new THREE.Vector2();
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var ticker = 0;
 
 var update = function(){
 
+    // RAY
+    raycaster.setFromCamera( mouse, camera );
+
+	// calculate objects intersecting the picking ray
+	var intersects = raycaster.intersectObjects( scene.children );
+
+	for ( var i = 0; i < intersects.length; i++ ) {
+
+		rayPos.x = intersects[i].point.x;
+        rayPos.y = intersects[i].point.z;
+
+        console.log(rayPos.x+" - "+rayPos.y);
+
+    }
+    
+    // UPDATE TARGETS
     ticker ++;
-    if (ticker % 60 == 0){
+    if (ticker % 10 == 0){
 
         for (var x = 0;x < geometry.vertices.length;x++){
 
             targetPos[x] = new THREE.Vector3(startPos[x].x,startPos[x].y,startPos[x].z);
-            targetPos[x].x += (Math.random()-0.5)*0.1;
-            targetPos[x].y += (Math.random()-0.5)*0.1;
-            targetPos[x].z += (Math.random()-0.5)*0.1;
+            targetPos[x].x += (Math.random()-0.5)*0.2;
+            targetPos[x].y += (Math.random()-0.5)*0.2;
+            targetPos[x].z += (Math.random()-0.5)*0.2;
+
+            var totalDist = startPos[x].distanceTo(new THREE.Vector3(rayPos.x,0,rayPos.y));
+
+            var newPos = 3-totalDist;
+            // newPos *= 2;
+            if (newPos < 0){newPos = 0;}
+
+            targetPos[x].y += newPos;
 
         }
 
     }
 
+    // SMOOTH MOVEMENT
     for (var x = 0;x < ((xSize*ySize)*6);x++){
 
         var currPos = geometry.vertices[x];
